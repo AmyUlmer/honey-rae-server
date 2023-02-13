a4d70b095572d6821fec92c612d0bcc35d3e302c
Amy Ulmer <aulmer703@gmail.com>
2023-02-13 14:18:53 -0600

    completed chapter 7

---
+++ repairsapi/views/ticket_view.py
@@ -0,0 +1,45 @@
+"""View module for handling requests for employee data"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from repairsapi.models import ServiceTicket
+
+
+class ServiceTicketView(ViewSet):
+    """Honey Rae API employees view"""
+
+    def list(self, request):
+        """Handle GET requests to get all employees
+        Returns:
+            Response -- JSON serialized list of employees
+        """
+        service_tickets = []
+
+        #employee can view all tix. customers requesting own tix.
+        if request.auth.user.is_staff:
+            service_tickets = ServiceTicket.objects.all()
+        else: 
+            service_tickets = ServiceTicket.objects.filter(customer__user=request.auth.user)
+
+        serialized = ServiceTicketSerializer(service_tickets, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single service ticket
+        Returns:
+            Response -- JSON serialized service ticket record
+        """
+
+        service_ticket = ServiceTicket.objects.get(pk=pk)
+        serialized = ServiceTicketSerializer(service_ticket, context={'request': request})
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+class ServiceTicketSerializer(serializers.ModelSerializer):
+    """JSON serializer for tickets"""
+    class Meta:
+        model = ServiceTicket
+        fields = ('id', 'customer', 'employee', 'description', 'emergency', 'date_completed')
+        depth = 1 #expands 
+
