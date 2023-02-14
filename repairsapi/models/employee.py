af5a3512893ddc691d7b54ca369b3e41b30e213d
Amy Ulmer <aulmer703@gmail.com>
2023-02-14 11:08:07 -0600

    added create tickets

--- repairsapi/models/employee.py
+++ repairsapi/models/employee.py
@@ -6,6 +6,8 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     specialty = models.CharField(max_length=155)
 
+    #property decorator.
+    #custom property on employee instance
     @property
-    def full_name(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+    def full_name(self): #property you want on an employee to send back to the client. 
+        return f'{self.user.first_name} {self.user.last_name}' #self = employee instance itself
--- repairsapi/views/ticket_view.py
+++ repairsapi/views/ticket_view.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from repairsapi.models import ServiceTicket
+from repairsapi.models import ServiceTicket, Employee, Customer
 
 
 class ServiceTicketView(ViewSet):
@@ -34,12 +34,40 @@ class ServiceTicketView(ViewSet):
         service_ticket = ServiceTicket.objects.get(pk=pk)
         serialized = ServiceTicketSerializer(service_ticket, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        """Handle POST requests for service tickets
 
+        Returns:
+            Response: JSON serialized representation of newly created service ticket
+        """
+        new_ticket = ServiceTicket()
+        new_ticket.customer = Customer.objects.get(user=request.auth.user)
+        new_ticket.description = request.data['description']
+        new_ticket.emergency = request.data['emergency']
+        new_ticket.save()
+
+        serialized = ServiceTicketSerializer(new_ticket, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+#create serializer for 'employee' field
+
+class TicketEmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('id', 'specialty', 'full_name', )
 
 class ServiceTicketSerializer(serializers.ModelSerializer):
     """JSON serializer for tickets"""
+
+    #how you want employee to be serialized
+    #for the employee field I want to use the TicketEmployee Serializer
+    employee = TicketEmployeeSerializer(many=False)
+
+
     class Meta:
         model = ServiceTicket
         fields = ('id', 'customer', 'employee', 'description', 'emergency', 'date_completed')
-        depth = 1 #expands 
+        depth = 2 #expands foreign keys (customer,employee)
 
